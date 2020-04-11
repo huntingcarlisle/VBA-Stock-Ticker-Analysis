6abb0ce555d600e0f213ee7b1d7cfeafad52b8e9
Hunter Carlisle <huntingcarlisle@gmail.com>
2020-04-11 14:14:30 -0700

    hi!

---
+++ main.py
@@ -0,0 +1,9 @@
+# @TODO: Write a function that returns the arithmetic average for a list of numbers
+def average(number_list):
+    pass
+
+# Test your function with the following:
+assert average([1]) == 1
+assert average([1, 8, 9]) == 6.0
+assert average([2, 9, 10]) == 7.0
+assert average([1,3]) == 2.0
